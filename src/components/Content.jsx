18c1199e15bd83b9e21848424b81f6680596ba51
ananthanandanan <ananthanandanan@gmail.com>
2023-09-17 23:22:33 +0530

    Click and useState

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react'
+
 function Content () {
+    const [name, setName] = useState("Ron");
+    const [count, setCount] = useState(0);
     const handleNameChange = () => {
         const name = ["Ron", "Kevin", "jacob"]
         const int = Math.floor(Math.random() * name.length)
-        return name[int]
+        setName(name[int])
       }
+    const handleCountChange = () => {
+        setCount(count + 1)
+      }
+
   return (
-    <div className="content">
-      <p>Hello {handleNameChange()}</p>
-    </div>
+    <main className="content">
+        <button onClick={handleNameChange}>Change Name</button>
+      <p>Hello {name}</p>
+        <button onClick={handleCountChange}>Change Count</button>
+        <p>Count: {count}</p>
+    </main>
   )
 }
 
